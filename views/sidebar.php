6063c41ac1690aee85e2b9b4e79e8b7f5d6c8d15
zubiai <41428060+zubiai@users.noreply.github.com>
2019-01-21 18:57:51 +0100

    Update sidebar.php

--- views/sidebar.php
+++ views/sidebar.php
@@ -1,66 +1,35 @@
-<?php
-/**
-* Donations Sidebar
-*/
-?>
-<!-- Improve Your Site -->
+<!-- Generate API Key -->
 <div class="postbox">
     <h3 class="hndle">
-        <span><?php _e('Improve Your Site', $this->plugin->name); ?></span>
+        <span><?php _e('Generate Woocommerce API key', $this->plugin->name); ?></span>
     </h3>
 
     <div class="inside">
         <p>
-            <?php _e( 'Want to take your site to the next level? Check out our daily free WordPress tutorials on ', $this->plugin->name ); ?>
-            <a href="http://www.wpbeginner.com/?utm_source=wpadmin&utm_campaign=freeplugins"><?php _e( 'WPBeginner blog' ); ?></a>.
+            <?php _e( "zubi.ai requires your Woocommerce API key to function properly. If you haven't already, go ahead and generate it ", $this->plugin->name ); ?>
+            <a href="<?php get_site_url(); ?>/wp-admin/admin.php?page=wc-settings&tab=advanced&section=keys"><?php _e( 'here' ); ?></a>.
         </p>
+		
 
-        <p>
-            <?php _e( 'Some of our popular guides:', $this->plugin->name ); ?>
-        </p>
-
-        <ul>
-           <li><a href="http://www.wpbeginner.com/wordpress-performance-speed/?utm_source=wpadmin&utm_campaign=freeplugins" target="_blank">
-              <?php _e( 'Speed Up WordPress', $this->plugin->name ); ?>
-           </a></li>
-            <li><a href="http://www.wpbeginner.com/wordpress-security/?utm_source=wpadmin&utm_campaign=freeplugins" target="_blank">
-                <?php _e( 'Improve WordPress Security', $this->plugin->name ); ?>
-            </a></li>
-
-            <li><a href="http://www.wpbeginner.com/wordpress-seo/?utm_source=wpadmin&utm_campaign=freeplugins" target="_blank">
-                <?php _e( 'Boost Your WordPress SEO', $this->plugin->name ); ?>
-            </a></li>
+        <u>
+            <?php _e( 'The steps to complete are:', $this->plugin->name ); ?>
+        </u>
+		
+		<ul style="margin-top:6px">
+           <li>1. Generate the <a href="/wp-admin/admin.php?page=wc-settings&amp;tab=advanced&amp;section=keys">API key</a></li>
+            <li>2. Copy/paste the API key <a href="http://portal.zubi.ai" target="_blank">here</a></li>
         </ul>
-
     </div>
 </div>
 
-<!-- Donate -->
+<!-- GDPR -->
 <div class="postbox">
     <h3 class="hndle">
-        <span><?php _e( 'Our WordPress Plugins', $this->plugin->name); ?></span>
+        <span><?php _e( 'GDPR & Data Privacy', $this->plugin->name); ?></span>
     </h3>
     <div class="inside">
-        <p>
-            <?php _e( 'Like this plugin? Check out our other WordPress plugins:', $this->plugin->name ); ?>
+        <p><?php _e( 'The zubi.ai tracker use cookies to track visitor behaviour. It is advicable to inform your visitors of any such tracking, may it be from zubi or any other third party solution (such as google analytics, facebook or other.) Read more about the cookies used by zubi.ai ', $this->plugin->name ); ?><a href="http://portal.zubi.ai" target="_blank"><?php _e( 'here', $this->plugin->name ); ?></a>
         </p>
-        <p>
-            <a href="https://wordpress.org/plugins/wpforms-lite/" target="_blank">
-                <?php _e( 'WPForms', $this->plugin->name ); ?>
-            </a>
-            <?php _e( ' - Drag & Drop WordPress Form Builder', $this->plugin->name ); ?>
-        </p>
-        <p>
-            <a href="https://wordpress.org/plugins/google-analytics-for-wordpress/" target="_blank">
-                <?php _e( 'MonsterInsights', $this->plugin->name ); ?>
-            </a>
-            <?php _e( ' - Google Analytics Made Easy for WordPress', $this->plugin->name ); ?>
-        </p>
-        <p>
-            <a href="http://optinmonster.com/" target="_blank">
-                <?php _e( 'OptinMonster', $this->plugin->name ); ?>
-            </a>
-            <?php _e( ' - Best WordPress Lead Generation Plugin', $this->plugin->name ); ?>
-        </p>
+		<p><a href="http://portal.zubi.ai" target="_blank"><?php _e( 'Terms of Service', $this->plugin->name ); ?></a></p>
     </div>
 </div>
